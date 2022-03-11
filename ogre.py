fb17dd7536902f7cc5c24e5f34a373eddf9a7bc1
ShobyG <shoby@uw.edu>
2022-03-11 12:17:58 -0800

    new ogre

--- ogre.py
+++ ogre.py
@@ -12,16 +12,17 @@ class Ogre(Monster):
 
     def __update_values_from_db(self):
         """ Updates the parameters of an Ogre from dungeondb"""
+
         db = DungeonCharDb() #connects and accesses monster_stats table of dungeondb
-        self.hp_total = db.get_data("Ogre", "hp")
+        self.hp_total = db.get_data_monster("Ogre", "hp")
         self.hp = self.hp_total
-        self.attack_speed = db.get_data("Ogre", "attack_speed")
-        self.hit_chance = db.get_data("Ogre", "hit_chance")
-        self.damage_min = db.get_data("Ogre", "damage_min")
-        self.damage_max = db.get_data("Ogre", "damage_max")
-        self.chance_to_heal = db.get_data("Ogre", "chance_to_heal")
-        self.min_heal_point = db.get_data("Ogre", "min_heal_point")
-        self.max_heal_point = db.get_data("Ogre", "max_heal_point")
+        self.attack_speed = db.get_data_monster("Ogre", "attack_speed")
+        self.hit_chance = db.get_data_monster("Ogre", "hit_chance")
+        self.damage_min = db.get_data_monster("Ogre", "damage_min")
+        self.damage_max = db.get_data_monster("Ogre", "damage_max")
+        self.chance_to_heal = db.get_data_monster("Ogre", "chance_to_heal")
+        self.min_heal_point = db.get_data_monster("Ogre", "min_heal_point")
+        self.max_heal_point = db.get_data_monster("Ogre", "max_heal_point")
 
 
 
