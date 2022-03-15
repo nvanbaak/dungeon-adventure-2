4fac8360aa6c4cf9d7af13d8fe9c5674ee0d6d1b
ShobyG <shoby@uw.edu>
2022-03-15 10:48:25 -0700

    upadted view, model and controller code

---
+++ controller.py
@@ -0,0 +1,254 @@
+import model
+import sprite
+import pygame
+from time import *
+import threading
+
+class Controller:
+
+    def __init__(self):
+        # print("C | __init__ | Controller init calls init of Model")
+        self.model = model.Model()
+        pygame.init()
+
+    def accept_view_reference(self, view_ref):
+        self.view = view_ref
+        print(" ")
+
+    def get_room_data(self):
+        return self.model.get_curr_pos()
+
+    def move_left(self):
+        return self.model.move_left()
+
+    def move_right(self):
+        return self.model.move_right()
+
+    def move_upper(self):
+        return self.model.move_up()
+
+    def move_down(self):
+        return self.model.move_down()
+
+    def refresh_room(self):
+        return self.model.refresh_room()
+
+    def reset_default_characters(self):
+        # print("C | calls model.reset_default_characters() via Controller")
+        self.model.reset_default_characters()
+
+    def get_all_peices_on_board(self):
+        return self.model.dict.items()
+
+    def get_hero_dict_items(self):
+        return self.model.hero_dict.items()
+
+    def get_alphanumeric_position(self, rowcolumntuple):
+        # print(f"C | calls get_alphanumeric_position({rowcolumntuple}) via Model")
+        return self.model.get_alphanumeric_position(rowcolumntuple)
+
+    def get_numeric_notation(self, rowcol):
+        # print("C | calls get_numeric_notation via Sprite()")
+        return sprite.get_numeric_notation(rowcol)
+
+    def pre_move_validation(self, start_pos, end_pos):
+        return self.model.pre_move_validation(start_pos, end_pos)
+
+    def get_model_dict(self):
+        return self.model.get_dict()
+
+    def get_hero_dict(self):
+        return self.model.get_hero_dict()
+
+    def gather(self, obj, pos):
+        curr_pos = self.model.get_curr_pos()
+        if obj.name == "healing_potion_y":
+            obj.visible = False
+            curr_pos.heal = None
+            self.model.player.health_potions += 1
+            self.model.game_stats["Healing Potions"] = self.model.player.health_potions
+            self.view.show_health_button = True
+            self.view.health_button.pack()
+        if obj.name == "healing_potion_g":
+            obj.visible = False
+            curr_pos.heal = None
+            self.model.player.health_potions += 1
+            self.model.game_stats["Healing Potions"] = self.model.player.health_potions
+            self.view.show_health_button = True
+            self.view.health_button.pack()
+        if obj.name == "vision_potion":
+            obj.visible = False
+            curr_pos.vision = False
+            self.model.player.vision_potions += 1
+            self.model.game_stats["Vision Potions"] = self.model.player.vision_potions
+            self.view.vision = True
+            self.view.vision_button.pack()
+        if obj.name == "gremlin" or obj.name == "skeleton" or obj.name == "ogre":
+            p1 = threading.Thread(target=self.attack_monster, args=())
+            # the player starts attacking the monster
+            p1.start()
+            self.thread_to_attack_player() # the thread to make the monster attacking the player
+            if curr_pos.monster_obj.hp <= 0:
+                p1.stop = True
+                curr_pos.monster = ""
+                self.i_fought_a_monster = True # allows the player to exit the room
+
+        if obj.name == "abstraction_pillar":
+            if self.model.pillars["A"] == "":
+                obj.visible = False
+                curr_pos.pillar = ""
+                self.model.pillars["A"] = True
+                self.model.game_stats["Pillars"] = str(self.model.game_stats["Pillars"]) + "A "
+        if obj.name == "encapsulation_pillar":
+            if self.model.pillars["E"] == "":
+                obj.visible = False
+                curr_pos.pillar = ""
+                self.model.pillars["E"] = True
+                self.model.game_stats["Pillars"] = str(self.model.game_stats["Pillars"]) + "E "
+        if obj.name == "polymorphism_pillar":
+            if self.model.pillars["P"] == "":
+                obj.visible = False
+                curr_pos.pillar = ""
+                self.model.pillars["P"] = True
+                self.model.game_stats["Pillars"] = str(self.model.game_stats["Pillars"]) + "P "
+        if obj.name == "inheritance_pillar":
+            if self.model.pillars["I"] == "":
+                obj.visible = False
+                curr_pos.pillar = ""
+                self.model.pillars["I"] = True
+                self.model.game_stats["Pillars"] = str(self.model.game_stats["Pillars"]) + "I "
+        self.model.game_stats["Hit Points"] = self.model.player.hp
+
+    def gather_sounds(self):
+        curr_pos = self.model.get_curr_pos()
+        if curr_pos.heal == "y":
+            self.play("magic_harp")
+        if curr_pos.heal == "g":
+            self.play("magic_harp")
+        if curr_pos.vision == True:
+            self.play("magic_harp")
+        if curr_pos.pillar == "a":
+            self.play("pillar")
+        if curr_pos.pillar == "e":
+            self.play("pillar")
+        if curr_pos.pillar == "p":
+            self.play("pillar")
+        if curr_pos.pillar == "i":
+            self.play("pillar")
+        if curr_pos.monster == "Gremlin" or curr_pos.monster == "Ogre" or curr_pos.monster == "Skeleton":
+            self.play("monster")
+        if curr_pos.pit == True:
+            self.play("welcome_pit")
+        if self.model.player.hp <= 0:
+            self.play("game_over")
+            self.view.ask_new_game()
+
+    def combat(self):
+        curr_pos = self.model.get_curr_pos()
+        if curr_pos.monster == "Gremlin" or curr_pos.monster == "Skeleton" or curr_pos.monster == "Ogre":
+            print(f"Pre-battle hit points: Player: {self.model.player.hp} | {curr_pos.monster} | {curr_pos.monster_obj.hp}")
+            self.model.player.combat(curr_pos.monster_obj)
+            print(f"Post-battle hit points: Player: {self.model.player.hp} | {curr_pos.monster} | {curr_pos.monster_obj.hp}")
+            if self.model.player.hp <= 0:
+                self.model.game_stats["Hit Points"] = self.model.player.hp
+                self.view.update_score_label()
+                self.play("game_over")
+                self.view.ask_new_game()
+            return curr_pos.monster_obj.hp
+
+    def pit_fall(self):
+        self.model.player.fall_into_pit()
+        if self.model.player.hp <= 0:
+            self.model.game_stats["Hit Points"] = self.model.player.hp
+            self.view.update_score_label()
+            self.play("game_over")
+            self.view.ask_new_game()
+
+    def play(self, file):
+        filename = "audio/{}.wav".format(
+            file)
+        sound = pygame.mixer.Sound(filename)
+        pygame.mixer.Sound.play(sound)
+        pygame.mixer.music.stop()
+
+    def load_hit_points(self):
+        self.model.game_stats["Hit Points"] = self.model.player.hp
+
+    def expunge(self):
+        curr_pos = self.model.get_curr_pos()
+        if curr_pos.heal == "y":
+            curr_pos.heal = None
+        if curr_pos.heal == "g":
+            curr_pos.heal = None
+        if curr_pos.vision == True:
+            curr_pos.vision = False
+        if curr_pos.pillar == "a":
+            curr_pos.pillar = None
+        if curr_pos.pillar == "e":
+            curr_pos.pillar = None
+        if curr_pos.pillar == "p":
+            curr_pos.pillar = None
+        if curr_pos.pillar == "i":
+            curr_pos.pillar = None
+
+    def get_game_stats (self):
+        return self.model.game_stats
+
+    def use_vision_potion(self, room):
+        self.model.player.use_vision_potion()
+        # str_vision = self.model.dungeon.use_vision_potion(room)
+        return self.model.dungeon.vision_potion_rooms(room)
+
+    def use_health_potion(self):
+        self.model.player.use_health_potion()
+
+    def get_model(self):
+        return self.model
+
+    def set_model(self, saved_model):
+        self.model = saved_model
+
+    def attack_monster(self):
+        attack_speed_factor = self.attack_speed_factor("player")
+        if self.model.player.hp > 0:
+            self.model.announce(f"{self.model.player.name} is attacking")
+            self.model.player.attack_target(self.model.curr_pos.monster_obj)
+            sleep(attack_speed_factor)
+            if self.model.curr_pos.monster_obj is not None and self.model.curr_pos.monster_obj.hp <= 0:
+                self.model.player.hp = self.model.player.hp_total
+
+    def thread_to_attack_player(self):
+        # self.attack_player()
+        p2 = threading.Thread(target=self.attack_player, args=())
+        p2.start()
+        # monster has killed the player
+        if self.model.player.hp <= 0:
+             p2.stop = True
+             self.model.game_stats["Hit Points"] = self.model.player.hp
+             self.view.update_score_label()
+             self.play("game_over")
+             self.view.ask_new_game()
+
+    def attack_player(self):
+        while True:
+            attack_speed_factor = self.attack_speed_factor("monster")
+            if self.model.curr_pos.monster_obj.hp > 0:
+                self.model.announce(f"{self.model.curr_pos.monster} is attacking")
+                self.model.curr_pos.monster_obj.attack_target(self.model.player)
+                sleep(attack_speed_factor)
+                if self.model.player.hp <= 0:
+                    break
+            else:
+                break
+
+    def attack_speed_factor(self, who_str):
+        total_speed = self.model.player.attack_speed + self.model.curr_pos.monster_obj.attack_speed
+
+        if who_str  == "player":
+            attack_speed_factor = total_speed / self.model.player.attack_speed
+            return attack_speed_factor
+
+        if who_str == "monster":
+            attack_speed_factor = total_speed / self.model.curr_pos.monster_obj.attack_speed
+            return attack_speed_factor
+
---
+++ model.py
@@ -0,0 +1,110 @@
+from dungeon_builder import DungeonBuilder
+from hero import Hero
+from warrior import Warrior
+from priestess import Priestess
+from thief import Thief
+from ogre import Ogre
+from gremlin import Gremlin
+from skeleton import Skeleton
+import sprite
+from configurations import *
+# from game_observer import Publisher, Subscriber
+
+class Model:
+
+    # print("M | class variables initialized before __init__")
+    dict = {}
+    hero_dict = {}
+
+    def __init__(self, hero = "warrior"):
+        self.game = DungeonBuilder.build_single_dungeon()
+        self.dungeon = self.game[0]
+        self.curr_pos = self.dungeon.enter_dungeon()
+        if hero == "warrior":
+            self.player = Warrior("TestWarrior", self)
+        elif hero == "priestess":
+            self.player = Priestess("TestPriestess", self)
+        else:
+            self.player = Thief("TestThief", self)
+        print(self.dungeon.dungeon.winning_path)
+        # self.subscriber_m = Subscriber(self)
+        self.pillars = {"A": "", "E": "", "P": "", "I": ""}
+        self.game_stats = {"Hit Points": 0, "Pillars": "", "Healing Potions": 0, "Vision Potions": 0}
+        self.dungeon.update_monsters_to_room(self)
+
+    def announce(self, message):
+        print(message)
+
+    def get_curr_pos(self):
+        return self.curr_pos
+
+    def move_left(self):
+        self.curr_pos = self.curr_pos.left_room
+
+    def move_right(self):
+        self.curr_pos = self.curr_pos.right_room
+
+    def move_up(self):
+        self.curr_pos = self.curr_pos.upper_room
+
+    def move_down(self):
+        self.curr_pos = self.curr_pos.down_room
+
+    def reset_default_characters(self):
+        self.dict.clear()
+        self.hero_dict.clear()
+        for position, value in START_SPRITES_POSITION.items():
+            self.dict[position] = sprite.create_sprite(value)
+            self.dict[position].keep_reference(self)
+        self.hero_dict[HERO_POSITION] = sprite.create_sprite(HERO_SPRITE)
+        # print(f"M | {self.dict}")
+        # print(f"M | {self.hero_dict}")
+        self.refresh_room()
+
+    def refresh_room(self):
+        for position, value in self.dict.items():
+            spr = value
+            if value.name == "abstraction_pillar" and self.curr_pos.pillar == "a":
+                spr.visible = True
+            if value.name == "encapsulation_pillar" and self.curr_pos.pillar == "e":
+                spr.visible = True
+            if value.name == "polymorphism_pillar" and self.curr_pos.pillar == "p":
+                spr.visible = True
+            if value.name == "inheritance_pillar" and self.curr_pos.pillar == "i":
+                spr.visible = True
+            if value.name == "pit" and self.curr_pos.pit == True:
+                spr.visible = True
+            if value.name == "healing_potion_y" and self.curr_pos.heal == "y":
+                spr.visible = True
+            if value.name == "healing_potion_g" and self.curr_pos.heal == "g":
+                spr.visible = True
+            if value.name == "vision_potion" and self.curr_pos.vision == True:
+                spr.visible = True
+            if value.name == "gremlin" and self.curr_pos.monster == "Gremlin":
+                spr.visible = True
+            if value.name == "skeleton" and self.curr_pos.monster == "Skeleton":
+                spr.visible = True
+            if value.name == "ogre" and self.curr_pos.monster == "Ogre":
+                spr.visible = True
+
+    def get_alphanumeric_position(self, rowcol):
+        if self.is_on_board(rowcol):
+            row, col = rowcol
+            return "{}{}".format(X_AXIS_LABELS[col], Y_AXIS_LABELS[row])
+
+    def is_on_board(self, rowcol):
+        row, col = rowcol
+        return 0 <= row <= 6 and 0 <= col <= 6
+
+    def pre_move_validation(self, initial_pos, final_pos):
+        self.move(initial_pos, final_pos)
+
+    def move(self, start_pos, final_pos):
+        # print(f"M | move(start, final) | self[{final_pos}] = self.dict.pop({start_pos}, None)")
+        self.hero_dict[final_pos] = self.hero_dict.pop(start_pos, None)
+
+    def get_dict(self):
+        return self.dict
+
+    def get_hero_dict(self):
+        return self.hero_dict
